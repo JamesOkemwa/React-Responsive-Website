065fad4ecffdcd6466cdc134dd765e28b20132b3
JamesOkemwa <okejames55@gmail.com>
2021-06-15 15:15:18 +0300

    Pricing Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer/Footer'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/HomePage/Home';
+import Services from './components/Services/Services'
+import Products from './components/Products/Products'
+import SignUp from './components/SignUp/SignUp'
 
 function App() {
   return (
@@ -9,8 +13,12 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' component={Home} />
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
         </Switch>
+        <Footer />
       </Router>
     </div>
   );
---
+++ src/components/Products/Data.js
@@ -0,0 +1,51 @@
+export const homeObjOne = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Exclusive Access',
+    headline: 'Unlimited transactions with limited fees',
+    description: 'Get access to our exclusive diamond card that allows you to send unlimited transactions without getting charged any fees',
+    buttonLabel: 'Get Started',
+    imgStart: '',
+    img: 'images/svg-1.svg',
+    alt: 'Credit Card'
+}
+
+export const homeObjTwo = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: '100% Secure',
+    headline: 'Stay protected 24/7 anywhere, anytime',
+    description: 'We have you covered no matter where youre located. Over 140 locations worldwide to ensure you have acces anytime',
+    buttonLabel: 'Learn More',
+    imgStart: '',
+    img: 'images/svg-5.svg',
+    alt: 'Credit Card'
+}
+
+export const homeObjThree = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Easy Setup',
+    headline: 'Super fast and simple onboarding process',
+    description: 'Get everything setup and running in less than 10 minutes. ALl you need to do is add your information and youre ready to go',
+    buttonLabel: 'Start Now',
+    imgStart: 'start',
+    img: 'images/svg-7.svg',
+    alt: 'Credit Card'
+}
+
+export const homeObjFour = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Data Analytics',
+    headline: 'Every transaction is stored on our secure cloud database',
+    description: 'Never have to worry again about sent receipts. We store your data so you can access it anytime',
+    buttonLabel: 'Sign Up Now',
+    imgStart: 'start',
+    img: 'images/svg-8.svg',
+    alt: 'Credit Card'
+}
